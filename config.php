f2dc1bd42afb5907f90d2daa5bb4c5fc95757512
LukeBeer <eat.lemons@gmail.com>
2014-02-24 18:43:36 +0000

    Refactored already?!

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
-require_once '../Broadworks_OCI-P/common.php';
-
+define('OCIP_BASEPATH', '../Broadworks_OCI-P/');
 define('OCIP_HOST', 'https://example.com/webservice/services/ProvisioningService');
 define('OCIP_USER', '');
 define('OCIP_PASS', '');
+
+require_once OCIP_BASEPATH . 'common.php';
