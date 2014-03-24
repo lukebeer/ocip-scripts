5a2d5b17668d7548cf3da95574960be9bc88510f
LukeBeer <eat.lemons@gmail.com>
2014-03-24 13:14:55 +0000

    Service Pack Assignment first push

---
+++ servicePackAssignment.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Service Packs assignment per user in service provider
+ * Date: 24/03/14
+ * Time: 12:30
+ */
+require_once '../Broadworks_OCI-P/common.php';
+Factory::getOCISchemaSystem();
+$client = CoreFactory::getOCIClient(OCIP_HOST);
+$client->login(OCIP_USER, OCIP_PASS);
+if (!isset($argv[1])) die("No service provider name provided as argument to script\n");
+if ($client->send(OCISchemaUser::UserGetListInServiceProviderRequest($argv[1]))) {
+    if (!$client->getResponse()) die("No users in $argv[1]\n");
+    foreach ($client->getResponse()->userTable['row'] as $item) {
+        $userId = $item['col'][0];
+        $client->send(OCISchemaUser::UserServiceGetAssignmentListRequest($userId));
+        $sps = null;
+        foreach ($client->getResponse()->servicePacksAssignmentTable['row'] as $row) {
+            if ($row['col'][1] == 'true') {
+                $sps[] = $row['col'][0];
+            }
+        }
+        if ($sps) echo "$userId,".implode(',',$sps)."\n";
+    }
+}
